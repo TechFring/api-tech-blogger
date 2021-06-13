627a7eb5e807be7f2a1428b0d7eec8c4cdec95f3
Danilo Miranda <danilo030920@gmail.com>
2021-06-13 12:05:18 -0300

    itens exibidos na tabela alterados

--- users/admin.py
+++ users/admin.py
@@ -4,12 +4,19 @@ from django.contrib.auth import admin as auth_admin
 from .forms import UserChangeForm, UserCreationForm
 from .models import User
 
-# Register your models here.
+
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     model = User
+    list_display = (
+        "username",
+        "first_name",
+        "total_publications",
+        "is_staff",
+        "is_superuser",
+    )
     fieldsets = auth_admin.UserAdmin.fieldsets + (
         ("Campos personalizados", {"fields": ("bio", "photo")}),
     )
