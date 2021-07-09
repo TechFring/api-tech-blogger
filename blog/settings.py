5fcd08ce8082f0d6c7ad4d376d823fa278860f8d
Danilo Miranda <danilo030920@gmail.com>
2021-07-09 10:47:19 -0300

    correção deploy

--- blog/settings.py
+++ blog/settings.py
@@ -86,15 +86,15 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # DEV
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # PROD
-DATABASES = {"default": dj_database_url.config()}
+# DATABASES = {"default": dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
